45ac232757040ca65ca8945f122b81c4c4afabe1
AlexOfili <ofilia@stpaulsschool.org.uk>
2026-07-23 12:20:29 +0100

    added unit converter (lengths)

--- project.py
+++
@@ -1,7 +0,0 @@
-# Tip Splitter
-def split_tip(bill, tip_percent, people):
-    """Work out what each person owes, including tip."""
-    total = bill * (1 + tip_percent / 100)
-    return round(total / people, 2)
-
-print(split_tip(100, 15, 4))  # Example usage
